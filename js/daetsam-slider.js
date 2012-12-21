ab9189cb55a2e387c4a652a9de0f2dc9ea76662c
Carlos Saito <k_saito_91@hotmail.com>
2012-12-21 15:06:25 +0100

    Plugin daetsam-slider

---
+++ js/daetsam-slider.js
@@ -0,0 +1,40 @@
+/*
+	Plugin daetsam-slider
+*/
+(function($) {
+	$.daetsam_slider = {
+		defaults: {}
+	};
+	$.fn.extend({
+		daetsam_slider: function() {
+			var active = 0;
+			
+			var contentList = $(this).find('ul');
+			var amount = contentList.find('li').length;
+			
+			var bulletList = $('<ul class="bullet-list clearfix"></ul>')
+			for (var i=0; i<amount; i++) {
+				var newBullet = $('<li><a href="javascript:;">&nbsp;</a></li>');
+				bulletList.append(newBullet);
+			}
+			
+			$(this).append(bulletList);
+			
+			bulletList.find('li:eq(0)').addClass('active-trail');
+			contentList.find('li:gt(0)').hide();
+			
+			bulletList.find('li').click(function(e) {
+				var id = $(this).index();
+				
+				if (active!=id) {
+					contentList.find('li:eq(' + active + ')').hide('fade'); //'slide', {direction: 'left'}, '500');
+					contentList.find('li:eq(' + id + ')').show('fade'); //'slide', {direction: 'left'}, '500');
+					active = id;
+					
+					bulletList.find('li').removeClass('active-trail');
+					bulletList.find('li:eq(' + id + ')').addClass('active-trail');
+				}
+			})
+		}
+	});
+})(jQuery);
