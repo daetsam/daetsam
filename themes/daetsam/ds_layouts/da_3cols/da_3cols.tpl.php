6182f3e701bd430089dfd207b090f4d8eedd55f4
otiasc <carlos.saito@outlook.com>
2013-02-02 16:44:37 +0100

    Plantilla display suite para 3 columnas

---
+++ themes/daetsam/ds_layouts/da_3cols/da_3cols.tpl.php
@@ -0,0 +1,25 @@
+<div class="<?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  
+  <?php if ($left): ?>
+    <div class="<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($center): ?>
+    <div class="<?php print $center_classes; ?>">
+      <?php print $center; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div class="<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </div>
+  <?php endif; ?>
+  
+</div>
