799a43dda1155a89253d22c36dd288898847a366
Subido desde Delegación <web@daetsam.es>
2013-03-20 13:46:19 +0100

    Cambios en el foro

--- themes/daetsam/advanced_forum_styles/daetsam_ic/advanced_forum-topic-list-view.tpl.php
+++ themes/daetsam/advanced_forum_styles/daetsam_ic/advanced_forum-topic-list-view.tpl.php
@@ -21,7 +21,7 @@
     <h2 class="sin-margen-superior"><?php print $row['title']; ?></h2>
     <div class="clearfix con-margen-inferior">
       <div class="grid-2 alpha">Creado por <?php print $row['name_1']; ?></div>
-      <div class="grid-2"><?php print $row['comment_count']; ?> respuestas</div>
+      <div class="grid-1"><?php print $row['comment_count']; ?> respuestas</div>
       <div class="grid-3 omega">Último post: <?php print $row['last_updated']; ?></div>
     </div>
   </li>
