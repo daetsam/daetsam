50aac68899a38dd0ff58628c145c2743ad0f06fb
Subido desde Delegación <web@daetsam.es>
2013-11-07 15:54:14 +0100

    Reactualización de módulos

---
+++ themes/daetsam/templates/maintenance-page.20131022.tpl.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
+<html lang="es" dir="ltr" version="HTML+RDFa 1.1"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://daetsam.aq.upm.es/sites/default/files/favicon.png" type="image/png" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+<meta about="/info/daetsam" property="sioc:num_replies" content="0" datatype="xsd:integer" />
+<link rel="shortlink" href="/node/6" />
+<meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+<link rel="canonical" href="/info/daetsam" />
+<meta content="Información general" about="/info/daetsam" property="dc:title" />
+<title>En desarrollo</title>
+<style type="text/css" media="all">
+@import url("http://daetsam.aq.upm.es/modules/system/system.base.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/modules/system/system.messages.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/modules/system/system.theme.css?mh30cj");
+</style>
+<style type="text/css" media="all">
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/alpha-reset.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/alpha-mobile.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/alpha-alpha.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/formalize.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/omega-text.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/omega-branding.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/omega-menu.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/omega-forms.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/omega/css/omega-visuals.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/default/files/fontyourface/wysiwyg.css?mh30cj");
+</style>
+<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=News+Cycle:700&amp;subset=latin" media="all" />
+<style type="text/css" media="all">
+@import url("http://daetsam.aq.upm.es/sites/default/files/fontyourface/local_fonts/News_Cycle-normal-normal/stylesheet.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/default/files/fontyourface/local_fonts/Steelfish-normal-normal/stylesheet.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/global.css?mh30cj");
+</style>
+
+<!--[if (lt IE 9)&(!IEMobile)]>
+<style type="text/css" media="all">@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default-normal.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/grid/alpha_default/normal/alpha-default-normal-12.css?mh30cj");</style>
+<![endif]-->
+
+<!--[if gte IE 9]><!-->
+<style type="text/css" media="all and (min-width: 740px) and (min-device-width: 740px), (max-device-width: 800px) and (min-width: 740px) and (orientation:landscape)">
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default-narrow.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/grid/alpha_default/narrow/alpha-default-narrow-12.css?mh30cj");
+</style>
+
+<!--<![endif]-->
+
+<!--[if gte IE 9]><!-->
+<style type="text/css" media="all and (min-width: 980px) and (min-device-width: 980px), all and (max-device-width: 1024px) and (min-width: 1024px) and (orientation:landscape)">
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default-normal.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/grid/alpha_default/normal/alpha-default-normal-12.css?mh30cj");
+</style>
+<!--<![endif]-->
+
+<!--[if gte IE 9]><!-->
+<style type="text/css" media="all and (min-width: 1220px)">
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/daetsam/css/daetsam-alpha-default-wide.css?mh30cj");
+@import url("http://daetsam.aq.upm.es/sites/all/themes/omega/alpha/css/grid/alpha_default/wide/alpha-default-wide-12.css?mh30cj");
+</style>
+<!--<![endif]-->
+<link rel='stylesheet' type='text/css' href='/B1D671CF-E532-4481-99AA-19F420D90332/netdefender/hui/ndhui.css' />
+</head>
+<body class="html not-front logged-in page-node page-node- page-node-6 node-type-static-page context-info">
+<script type='text/javascript' language='javascript' src='/B1D671CF-E532-4481-99AA-19F420D90332/netdefender/hui/ndhui.js?0=0&amp;0=0&amp;0=0'></script>
+<div id="skip-link"> <a href="#main-content" class="element-invisible element-focusable">Pasar al contenido principal</a> </div>
+<div class="region region-page-top" id="region-page-top">
+  <div class="region-inner region-page-top-inner"> </div>
+</div>
+<div class="page clearfix" id="page">
+  <header id="section-header" class="section section-header">
+    <div id="zone-menu-wrapper" class="zone-wrapper zone-menu-wrapper clearfix zon-menu-wrapper">
+      <div id="zone-menu" class="zone zone-menu clearfix equal-height-container container-12">
+        <div class="grid-2 region region-logo equal-height-element reg-logo" id="region-logo">
+          <div class="block-image"><a href="/"><img src="/sites/all/themes/daetsam/images/logodaetsam.png" alt="Daetsam" title="Inicio" /></a></div>
+        </div>   
+      </div>  
+    </div>
+    <div id="zone-menu-bottom-wrapper" class="zone-wrapper zone-menu-bottom-wrapper clearfix">
+      <div id="zone-menu-bottom" class="zone zone-menu-bottom clearfix zon-menu-bottom container-12">
+        <div class="grid-6 region region-menu-bottom-first" id="region-menu-bottom-first">
+          <div class="region-inner region-menu-bottom-first-inner">
+            <div class="block-inner clearfix">
+              <p>Delegación de Alumnos Etsam</p>
+              <a id='context-block-block-4' class='context-block editable edit-contexto-global'></a>
+            </div>
+          </div>
+        </div>
+        <div class="grid-6 region region-menu-bottom-second reg-menu-bottom-second" id="region-menu-bottom-second">
+          <div class="region-inner region-menu-bottom-second-inner">
+            <div class="block block-block block-5 block-block-5 odd block-without-title" id="block-block-5">
+              <div class="block-inner clearfix">
+                <div class="content clearfix">
+                  <p><a href="mailto:daetsam@daetsam.es">daetsam@daetsam.es</a> •  tel.: 91 336 6505  •  Av. Juan de Herrera nº4 28040</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="grid-12 region">
+          <hr />
+        </div>
+      </div>
+    </div>
+    <div id="zone-header-wrapper" class="zone-wrapper zone-header-wrapper clearfix">
+      <div id="zone-header" class="zone zone-header clearfix zon-header container-12">
+        <div class="grid-12 region region-header-top reg-header-top" id="region-header-top">
+          <div class="content clearfix">
+            <h1>En desarrollo...</h1>
+          </div>
+        </div>
+        <div class="grid-12 region region-header-bottom reg-header-bottom" id="region-header-bottom">
+          <hr />
+        </div>
+      </div>
+    </div>
+  </header>
+  <section id="section-content" class="section section-content">
+    <div class="zone zone-content clearfix zon-content container-12">
+      <div class="grid-12 region reg-content">
+        <p>Estamos trabajando en la nueva web de Delegación de Alumnos de la ETSAM.</p>
+        <p>Mientras puedes visitar la web antigua: <a href="http://www.aq.upm.es/Alumnos/daetsam">daetsam.es</a></p>
+      </div>
+    </div>
+  </section>
+  <footer id="section-footer" class="section section-footer">
+    <div id="zone-footer-wrapper" class="zone-wrapper zone-footer-wrapper clearfix">
+      <div id="zone-footer" class="zone zone-footer clearfix zon-footer container-12">
+        <div class="grid-12 region region-footer-top reg-footer-top" id="region-footer-top">
+          <hr />
+        </div>
+        <div class="grid-6 region region-footer-first reg-footer-first" id="region-footer-first">
+          <div class="region-inner region-footer-first-inner">
+            <div class="block block-block block-8 block-block-8 odd block-without-title" id="block-block-8">
+              <div class="block-inner clearfix">
+                <div class="content clearfix"> <a href="http://www.aq.upm.es/Alumnos/daetsam/">Página antigua</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="grid-6 region region-footer-second reg-footer-second" id="region-footer-second">
+          <div class="region-inner region-footer-second-inner">
+            <div class="block block-block block-9 block-block-9 odd block-without-title" id="block-block-9">
+              <div class="block-inner clearfix">
+                <div class="content clearfix">
+                  <div class="block-image"><a href="#region-page-top"><img src="/sites/all/themes/daetsam/images/logodaetsamcompleto.png" alt="Daetsam" title="Volver arriba" /></a></div>
+                  <a id='context-block-block-9' class='context-block editable edit-contexto-global'></a> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
